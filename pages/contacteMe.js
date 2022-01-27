1c6848c45e80ed713d59f8e39ae5114045516a95
ouadjih <47463757+ouadjih@users.noreply.github.com>
2022-01-27 14:28:42 +0100

    fixed some uppercase naming

---
+++ pages/contacteMe.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class contacteMe extends Component {
+  render() {
+    return <div>contact me page</div>;
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import { Stack, Spacer } from '@chakra-ui/react'
-import Hero from '../components/Home/Hero'
-import About from '../components/Home/About'
-import Services from '../components/Home/Services'
-import Stats from '../components/Home/Numbers'
-import Projects from '../components/Home/Projects'
-import Testimonials from '../components/Home/Testimonials'
+import Hero from '../components/home/Hero'
+import About from '../components/home/About'
+import Services from '../components/home/Services'
+import Stats from '../components/home/Numbers'
+import Projects from '../components/home/Projects'
+import Testimonials from '../components/home/Testimonials'
 
 export default function Index() {
   return (
